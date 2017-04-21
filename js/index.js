8d33007de98442288ba12d1dc0983990079f8b4b
LuoJie <jieluo@tcl.com>
2017-04-21 19:37:35 +0800

    jQuery test code stage 1

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$('input').click(function(event) {
+		/* Act on the event */
+		alert('click me');
+	});
+	// $('#box').css({
+	// 	color: 'red',
+	// 	'font-size': '16px',
+	// });
+
+	// jQuery('#box').append('<p>Some text</p>');
+	// $('p').css({
+	// 	color: 'green',
+	// });
+
+	//alert($('#box').get(0));
+	//alert($('#box').width(200));
+
+	//jQuery.noConflict();
+	//var $$=jQuery;
+
+	//alert($$('input').get(0));
+
+	$('.pox').css('color', '#ccc');
+
+	//alert($('li').length);
+
+	$('#box > p').css('color', 'blue');
+
+
+
+
+
+
+
+
+
+
+});
